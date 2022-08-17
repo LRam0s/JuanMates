b5df6f0892832a62bfcbd2b19ff30dbcc652711c
Lucio Ramos <lucio.a.ramos@gmail.com>
2022-08-17 16:55:53 -0300

    Se parcha el Header

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ItemListContainer from "./ItemListContainer/ItemListContainer";
+import estilos from "./Main.module.css";
 
 const Main = () => {
   return (
-    <ItemListContainer text="Bienvenido a JuanMates, acá vas a poder encontrar los mejores productos al menor precio del mercado." />
+    <main>
+      <section className={estilos.space}></section>
+      <ItemListContainer text="Bienvenido a JuanMates, acá vas a poder encontrar los mejores productos al menor precio del mercado." />
+    </main>
   );
 };
 
