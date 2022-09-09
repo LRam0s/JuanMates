63d3339582f73f97b5d10d6e35dd061472d4512a
Lucio Ramos <lucio.a.ramos@gmail.com>
2022-09-09 16:57:24 -0300

    Se agregan las logicas del cartContext y se visualizan en cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import estilos from "./Cart.module.css";
+import Button from "../Button/Button";
+import { BsTrash } from "react-icons/bs";
 
 const Cart = () => {
-  return <div>Bienvenidos al carrito</div>;
+  const { cart, clear, removeItem } = useContext(CartContext);
+  return (
+    <section className={estilos.container}>
+      {cart.map((item) => {
+        return (
+          <div key={item.id} className={estilos.cart}>
+            <img style={{ height: "150px" }} src={item.img} alt="" />
+            <p>{item.title}</p>
+            <p> Cantidad: {item.cantidad}</p>
+            <p>Precio: {item.cantidad * item.price}</p>
+            <button
+              onClick={() => {
+                removeItem(item.id);
+              }}
+            >
+              <BsTrash size="20" />
+            </button>
+          </div>
+        );
+      })}
+      <div className={estilos.button}>
+        <Button btnText="Borrar carrito" fnButton={clear} />
+      </div>
+    </section>
+  );
 };
 
 export default Cart;
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import ItemCount from "../ItemCount/ItemCount";
 import estilos from "./ItemDetail.module.css";
+import { CartContext } from "../../context/CartContext";
 
 const ItemDetail = ({ items }) => {
   const [cantidad, setCantidad] = useState(0);
+  const { addItem } = useContext(CartContext);
 
   const onAdd = (cant) => {
     setCantidad(cant);
+    addItem(items, cant);
     cant === 1
       ? alert(`Se agregó ${cant} ${items.title} al carrito`)
       : alert(`Se agregaron ${cant} ${items.title} al carrito`);
